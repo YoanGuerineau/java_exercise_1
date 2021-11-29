ef74b176274e17ff45480e9ad6c6a8642d6191a3
E168339C <yoan.guerineau@etu.univ-nantes.fr>
2021-11-29 14:12:05 +0100

    Add Fibo command

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 
 public class Launcher {
+
     public static void main(String[] args) {
-        System.out.println("Bienvenue !");
+        System.out.println("Welcome !");
         Scanner scanner = new Scanner(System.in);
-        String saisie = scanner.nextLine();
-        while ( ! saisie.equals("quit") ) {
-            System.out.println("Unknown command");
-            saisie = scanner.nextLine();
+        String input = scanner.nextLine();
+        while ( ! input.equals("quit") ) {
+            if ( input.equals("fibo") ) {
+                System.out.print("Value:");
+                int value = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println(fibonacci(value));
+            } else {
+                System.out.println("Unknown command");
+            }
+            input = scanner.nextLine();
         }
     }
+
+    private static int fibonacci(int n) {
+        if ( n == 0 ) {
+            return 0;
+        } else if ( n == 1 ) {
+            return 1;
+        } else {
+            return fibonacci(n-1 ) + fibonacci( n-2 );
+        }
+    }
+
 }
