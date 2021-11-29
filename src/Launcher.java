e545428a89f1e53a264ceb4b761344b09e844fb5
E168339C <yoan.guerineau@etu.univ-nantes.fr>
2021-11-29 13:59:55 +0100

    Part 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,7 +5,9 @@ public class Launcher {
         System.out.println("Bienvenue !");
         Scanner scanner = new Scanner(System.in);
         String saisie = scanner.nextLine();
-        if ( ! saisie.equals("quit") )
+        while ( ! saisie.equals("quit") ) {
             System.out.println("Unknown command");
+            saisie = scanner.nextLine();
+        }
     }
 }
